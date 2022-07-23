b0aa6690c627d0ff5b5cf4aa12cc52224886a38c
FlowEV <murpurpower@gmail.com>
2022-07-23 14:24:48 +0300

    - Design - Updated every page

---
+++ app/src/main/java/com/example/beautystore/FaceFragment.java
@@ -0,0 +1,18 @@
+package com.example.beautystore;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FaceFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_face, container, false);
+    }
+}
